b2906ceea9710342f8466aba80c8b28576f60c6e
kimjaer <wofus0422@gmail.com>
2023-11-03 17:52:15 +0900

    A형 시작인데 css 미완성, html 미완성

--- js/web.js
+++ js/web.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
-    //화면이 준비되면 실행해라.
+
+    setInterval(function(){
+        
+    })
+    $(".swiper-wrapper").animate({"marginLeft" : -1200}, 400, function(){
+        $("div:first-child",this).appendTo($(this))
+        $(this).css("marginLeft", 0)
+    })
 
 })
