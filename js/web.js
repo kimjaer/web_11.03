3c3000e20ae48489aec4c238d6610dbc5ad364b3
kimjaer <wofus0422@gmail.com>
2023-11-03 18:00:02 +0900

    A형 자바

--- js/web.js
+++ js/web.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     setInterval(function(){
-        
-    })
-    $(".swiper-wrapper").animate({"marginLeft" : -1200}, 400, function(){
-        $("div:first-child",this).appendTo($(this))
-        $(this).css("marginLeft", 0)
-    })
+        $(".swiper-wrapper").animate({"marginLeft" : -1200}, 400, function(){
+            $("div:first-child",this).appendTo($(this))
+            $(this).css("marginLeft", 0)
+        })
+    
+    },3000)
 
 })
